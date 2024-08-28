cc0ccce30379fc67271f12a0b13c3935f07ca315
luan2610 <luanquynh2610@gmail.com>
2024-08-28 14:52:41 +0700

    bai tap28-8

---
+++ btbuoi3/Controllers/TheLoaiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace btbuoi3.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
