d1a591f736d133f6ef84c5d57accfc03654cc555
luan2610 <luanquynh2610@gmail.com>
2024-08-31 15:15:21 +0700

    BT gioi thieu

---
+++ BaiTap02_B/Controllers/TheLoaiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap02_B.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
