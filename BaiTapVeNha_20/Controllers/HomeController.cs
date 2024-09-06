51591b93045828bfce2e2559a9f4d49c5e538bc6
luan2610 <luanquynh2610@gmail.com>
2024-09-06 19:20:48 +0700

    hoan thanh baitapvenha

---
+++ BaiTapVeNha_20/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using BaiTapVeNha_20.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BaiTapVeNha_20.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult MayTinh(double a, double b, string pheptinh)
+        {
+            double ketqua = 0;
+
+            switch (pheptinh)
+            {
+                case "cong":
+                    ketqua = a + b;
+                    break;
+                case "tru":
+                    ketqua = a - b;
+                    break;
+                case "nhan":
+                    ketqua = a * b;
+                    break;
+                case "chia":
+
+                    if (b == 0)
+                    {
+                        ViewBag.Error = "Không thể chia cho 0";
+                        return View();
+                    }
+                    ketqua = a / b;
+                    break;
+                default:
+                    ViewBag.Error = "Phép tính không hợp lệ";
+                    return View();
+            }
+
+            ViewBag.KetQua = ketqua;
+            return View();
+        }
+    }
+}
---
+++ BaiTapVeNha_20/Controllers/Tuan02Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapVeNha_20.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Hoten = "Nguyễn Quốc Luân";
+            ViewBag.MSSV = "1822041279";
+            ViewData["Nam"] = "2004";
+            return View();
+        }
+    }
+}
