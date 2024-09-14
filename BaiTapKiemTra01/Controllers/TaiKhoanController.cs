5c472cb24542faf1ee72f8031c98389566d89f67
luan2610 <luanquynh2610@gmail.com>
2024-09-14 15:13:25 +0700

    hoan thanh bai kiem tra 01

---
+++ BaiTapKiemTra01/Controllers/TaiKhoanController.cs
@@ -0,0 +1,22 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+	public class TaiKhoanController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult DangKy(TaiKhoanViewModel taiKhoan )
+		{
+            if (taiKhoan.Username != null)
+            {
+                return Content(taiKhoan.Username);
+            }
+
+            return View();
+		}
+	}
+}
---
+++ BaiTapKiemTra01/Models/TaiKhoanViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BaiTapKiemTra01.Models
+{
+	public class TaiKhoanViewModel
+	{
+        public string hoten { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int tuoi { get; set; }
+    }
+}
