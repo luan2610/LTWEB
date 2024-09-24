fb02fb9d78be4ee03cacd5b22a764812ea9097ec
luan2610 <luanquynh2610@gmail.com>
2024-09-24 16:16:44 +0700

    hoan thanh bt07

---
+++ BT07/BT07/Controllers/TheLoaiController.cs
@@ -0,0 +1,87 @@
+﻿using BT07.Data;
+using BT07.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BT07.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TheLoaiController(ApplicationDbContext db)
+        {
+              _db = db;
+        }
+        public IActionResult Index()
+        {
+            var theloai = _db.TheLoai.ToList();
+            ViewBag.theloai = theloai;
+            return View();
+        }
+        [HttpGet]
+		public IActionResult Create()
+		{
+			
+			return View();
+		}
+        [HttpPost]
+		public IActionResult Create(TheLoai theloai)
+		{
+            if (ModelState.IsValid) 
+            {//thêm thông tin thể loại
+                _db.TheLoai.Add(theloai);
+                //lưu lại
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+			return View();
+		}
+        [HttpGet]
+		public IActionResult Edit(int Id)
+		{
+            if (Id == 0) 
+            {
+                return NotFound();
+            }
+            var theloai = _db.TheLoai.Find(Id);
+
+			return View();
+		}
+         [HttpPost]
+		public IActionResult Edit(TheLoai theloai)
+		{
+			if (ModelState.IsValid)
+			{//thêm thông tin thể loại
+				_db.TheLoai.Update(theloai);
+				//lưu lại
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+		[HttpGet]
+		public IActionResult Delete(int Id)
+		{
+			if (Id == 0)
+			{
+				return NotFound();
+			}
+			var theloai = _db.TheLoai.Find(Id);
+
+			return View();
+		}
+		[HttpPost]
+		public IActionResult DeleteConfirm(int id)
+		{
+			var theloai =_db.TheLoai.Find(id);
+			if (theloai == null)
+			{
+				return NotFound();
+			}
+			_db.TheLoai.Remove(theloai);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
+
---
+++ BT07/BT07/Models/TheLoai.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BT07.Models
+{
+    public class TheLoai
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="không được để trống tên thể loại!")]
+        [Display(Name ="Thể Loại")]
+        public string Name { get; set; }
+		[Required(ErrorMessage = "không đúng định dạng ngày tháng năm")]
+		[Display(Description = "Ngày Tạo")]
+		public DateTime Description { get; set; } = DateTime.Now;
+    }
+}
