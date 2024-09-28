e5782cb50e74260a4711a93d13377b3fb4605e67
luan2610 <luanquynh2610@gmail.com>
2024-09-28 15:24:05 +0700

    hoan thanh Bai Kiem Tra 02

---
+++ BaiKiemTra02/BaiKiemTra02/Controllers/LopHocController.cs
@@ -0,0 +1,38 @@
+﻿using BaiKiemTra02.Data;
+using BaiKiemTra02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiKiemTra02.Controllers
+{
+    public class LopHocController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public LopHocController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var lophoc = _db.lophoc.ToList();
+            ViewBag.lophoc = lophoc;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(LopHoc lophoc)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.lophoc.Add(lophoc);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        
+    }
+}
---
+++ BaiKiemTra02/BaiKiemTra02/Models/LopHoc.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BaiKiemTra02.Models
+{
+    public class LopHoc
+    {
+        
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Tên lớp học là bắt buộc.")]
+        public string TenLopHoc { get; set; }
+
+        [Range(2000, 2024, ErrorMessage = "Năm nhập học phải nằm trong khoảng từ 2000 đến 2024.")]
+        public int NamNhapHoc { get; set; }
+
+        [Range(2000, 3000, ErrorMessage = "Năm ra trường phải nằm trong khoảng từ 2000 đến 3000.")]
+        public int NamRaTruong { get; set; }
+
+        [Range(1, 5000, ErrorMessage = "Số lượng sinh viên phải nằm trong khoảng từ 1 đến 5000.")]
+        public int SoLuongSinhVien { get; set; }
+    }
+}
+
